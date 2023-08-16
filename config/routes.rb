bb62518b56c2d2be509497b2d909205bf81bf06f
Iyayi Roland <iyayiroland@gmail.com>
2023-08-16 06:10:37 +0200

    Add Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  patch 'tasks/:id', to: 'tasks#update'
 end
